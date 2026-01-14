738209b5b8c3c900928429f27181485cfe548152
Rangaswamyreddy Kappeta <kappetarangaswamyreddy@gmail.com>
2026-01-13 20:43:24 -0500

    Update site.ts

--- data/site.ts
+++ data/site.ts
@@ -3,7 +3,7 @@ export const site = {
   role: "RPA Developer (UiPath)",
   email: "kappetarangaswamyreddy@gmail.com",
   phone: "+1 (667) 345-4688",
-  linkedInLabel: "LinkedIn",
+  linkedInLabel: "Rangaswamyreddy Kappeta",
   linkedInUrl: "https://www.linkedin.com/in/ranga-swamy-reddy-4a0b79284/", // TODO: replace with your full LinkedIn URL
   location: "USA",
   keywords: [
